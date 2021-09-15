549663a12d140e724e4af3e43b064edad7f3af51
Madison Schirmer <90575585+m-schirmer@users.noreply.github.com>
2021-09-15 15:58:54 -0500

    Initial commit

---
+++ Learning Activity 4.appstudio/forms/Form1/Form1.js
@@ -0,0 +1,7 @@
+petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+withSal = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam", "Sal"]
+ console.log(withSal)
+// console.log(`The third pet is named ${petNames[2]}`)
+console.log(withSal.length)
+withSal.pop()
+console.log(withSal)
